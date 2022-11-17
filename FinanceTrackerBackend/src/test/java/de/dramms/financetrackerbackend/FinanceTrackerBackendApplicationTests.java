379437b83d221c600c07a563ec609af33789b9e9
dramms <dr@web-computing.de>
2022-11-17 21:35:58 +0100

    Create project

---
+++ FinanceTrackerBackend/src/test/java/de/dramms/financetrackerbackend/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dramms.financetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FinanceTracker
+
+# Backend
+- Mit Spring Boot, Java 17.
+- Datenbank: SQLite vorerst -> später tauschen.
+
+Umsetzen:
+- Logging
+- Userverwaltung
+
+# Frontend
